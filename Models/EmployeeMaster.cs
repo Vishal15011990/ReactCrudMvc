c1aa36cf9f7977e6594f448582f8f364b24c22f3
Vishal Kumar <kumar.preparestudy@outlook.com>
2020-08-11 09:59:44 +0530

    Commit

---
+++ Models/EmployeeMaster.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReactCrudMvc.Models
+{
+    public partial class EmployeeMaster
+    {
+        public int EmpId { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string EmailId { get; set; }
+        public string Address { get; set; }
+        public DateTime DateofBirth { get; set; }
+        public bool? IsActive { get; set; }
+        public bool? IsDelete { get; set; }
+        public DateTime Createdon { get; set; }
+        public Guid CreatedBy { get; set; }
+        public string Country { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public int? ZipCode { get; set; }
+    }
+}
---
+++ Models/ReactDbContext.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ReactCrudMvc.Models
+{
+    public partial class ReactDbContext : DbContext
+    {
+        public ReactDbContext()
+        {
+        }
+
+        public ReactDbContext(DbContextOptions<ReactDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<EmployeeMaster> EmployeeMaster { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-BUE1523;initial Catalog=ReactDb;integrated security=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<EmployeeMaster>(entity =>
+            {
+                entity.HasKey(e => e.EmpId);
+
+                entity.Property(e => e.Address)
+                    .IsRequired()
+                    .HasColumnType("text");
+
+                entity.Property(e => e.City)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Country).HasMaxLength(50);
+
+                entity.Property(e => e.Createdon).HasColumnType("datetime");
+
+                entity.Property(e => e.DateofBirth).HasColumnType("datetime");
+
+                entity.Property(e => e.EmailId)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Phone)
+                    .IsRequired()
+                    .HasMaxLength(10)
+                    .IsFixedLength();
+
+                entity.Property(e => e.State).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
